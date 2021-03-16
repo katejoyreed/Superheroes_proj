02e1a3e447cd6579ac75e69ef376612d8a3c8fca
katejoyreed <katejoyreed@gmail.com>
2021-03-16 14:23:50 -0500

    Added Initial migration

--- Superheroes/Data/ApplicationDbContext.cs
+++ Superheroes/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Superheroes.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +13,7 @@ namespace Superheroes.Data
             : base(options)
         {
         }
+
+        public DbSet<Sup> Sups { get; set; }
     }
 }
---
+++ Superheroes/Data/Migrations/20210316191524_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Superheroes.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sups",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    AlterEgo = table.Column<string>(nullable: true),
+                    PrimaryAbility = table.Column<string>(nullable: true),
+                    SecondaryAbility = table.Column<string>(nullable: true),
+                    Catchphrase = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sups", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sups");
+        }
+    }
+}
